0ab0b9e217831c5ba85063c9aa0c9d3a75f4dd23
Jaime Lopez <jeims.lopez@gmail.com>
2023-08-28 13:56:15 -0500

    Refactoring

--- mem/handler_windows.go
+++ mem/handler_windows.go
@@ -4,7 +4,7 @@ type platform_process struct {
 	handle windows.Handle
 }
 
-func (h *handler)) init() error {
+func (h *handler) init() error {
 	handle, err := windows.OpenProcess(windows.PROCESS_VM_OPERATION|windows.PROCESS_VM_READ|windows.PROCESS_VM_WRITE, false, uint32(h.ProcessID))
 	if err != nil {
 		return err
@@ -15,6 +15,6 @@ func (h *handler)) init() error {
 	return nil
 }
 
-func (h *handler)) close() {
+func (h *handler) close() {
 	_ = windows.CloseHandle(h.handle)
 }
